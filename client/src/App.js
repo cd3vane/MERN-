e58e84c9fb2e9bbbee23c23977387ea588d1f83b
Charles DeVane <devane.charles98@gmail.com>
2021-09-15 18:04:53 -0400

    added profile reducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Alert from './components/layout/Alert';
 import Login from './components/auth/Login';
+import PrivateRoute from './components/routing/PrivateRoute';
 import Register from './components/auth/Register';
+import Dashboard from './components/dashboard/Dashboard';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
-const  App = () => (
+if(localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const  App = () => { 
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
     <Provider store={store}>
       <Router>
         <Fragment>
@@ -21,11 +34,12 @@ const  App = () => (
             <Switch>
               <Route exact path="/register" component={ Register } />
               <Route exact path="/login" component={ Login } />
+              <PrivateRoute exact path="/dashboard" component={ Dashboard } />
             </Switch>
           </section>
         </Fragment>
       </Router>
     </Provider>  
-);
+)};
 
 export default App;
