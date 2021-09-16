ce298d4e3cf4250dd44c3f2cb0fe49946a700037
Charles DeVane <devane.charles98@gmail.com>
2021-09-16 14:01:11 -0400

    Filling out dashboard and exp/edu states

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import Landing from './components/layout/Landing';
 import Alert from './components/layout/Alert';
 import Login from './components/auth/Login';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
 import Register from './components/auth/Register';
 import Dashboard from './components/dashboard/Dashboard';
@@ -40,6 +43,21 @@ const App = () => {
                 exact
                 path='/create-profile'
                 component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/edit-profile'
+                component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path='/add-education'
+                component={AddEducation}
               />
             </Switch>
           </section>
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,6 +1,9 @@
 import React, { Fragment, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
@@ -25,69 +28,9 @@ const Dashboard = ({
 
       {profile !== null ? (
         <Fragment>
-          <div class='dash-buttons'>
-            <a href='edit-profile.html' class='btn btn-light'>
-              <i class='fas fa-user-circle text-primary'></i> Edit Profile
-            </a>
-            <a href='add-experience.html' class='btn btn-light'>
-              <i class='fab fa-black-tie text-primary'></i> Add Experience
-            </a>
-            <a href='add-education.html' class='btn btn-light'>
-              <i class='fas fa-graduation-cap text-primary'></i> Add Education
-            </a>
-          </div>
-
-          <h2 class='my-2'>Experience Credentials</h2>
-          <table class='table'>
-            <thead>
-              <tr>
-                <th>Company</th>
-                <th class='hide-sm'>Title</th>
-                <th class='hide-sm'>Years</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Tech Guy Web Solutions</td>
-                <td class='hide-sm'>Senior Developer</td>
-                <td class='hide-sm'>02-03-2009 - 01-02-2014</td>
-                <td>
-                  <button class='btn btn-danger'>Delete</button>
-                </td>
-              </tr>
-              <tr>
-                <td>Traversy Media</td>
-                <td class='hide-sm'>Instructor & Developer</td>
-                <td class='hide-sm'>02-03-2015 - Now</td>
-                <td>
-                  <button class='btn btn-danger'>Delete</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-
-          <h2 class='my-2'>Education Credentials</h2>
-          <table class='table'>
-            <thead>
-              <tr>
-                <th>School</th>
-                <th class='hide-sm'>Degree</th>
-                <th class='hide-sm'>Years</th>
-                <th />
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Northern Essex</td>
-                <td class='hide-sm'>Associates</td>
-                <td class='hide-sm'>02-03-2007 - 01-02-2009</td>
-                <td>
-                  <button class='btn btn-danger'>Delete</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <DashboardActions />
+          <Experience />
+          <Education />
           <div class='my-2'>
             <button class='btn btn-danger'>
               <i class='fas fa-user-minus'></i>
