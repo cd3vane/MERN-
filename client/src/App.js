b2486687022f9db593f469557cef29caa0c84f55
Charles DeVane <devane.charles98@gmail.com>
2021-09-17 19:53:29 -0400

    started posts component

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Register from './components/auth/Register';
 import Dashboard from './components/dashboard/Dashboard';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -64,6 +65,7 @@ const App = () => {
                 path='/add-education'
                 component={AddEducation}
               />
+              <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Fragment>
